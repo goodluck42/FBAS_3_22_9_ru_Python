de3c3cec13a09467df79de4d6a8a8b4f9743ba87
Alex <xrlogic1@gmail.com>
2022-11-29 15:30:18 +0400

    Initial commit

---
+++ DataTypes/main.py
@@ -0,0 +1,82 @@
+# int
+# float
+# str
+# bool
+
+
+# text: str = "Hello world"
+# number: int = 42
+# f_number: float = 1.0
+# boolean: bool = True  # True/False
+
+
+# str -> int
+
+# text = input("Enter a number: ")
+# result = int(text)
+#
+# print(result * 2)
+
+# str -> float
+
+# float_number = float(input("Enter a float number: "))
+#
+# print(float_number * 2)
+#
+# text = input("Enter a float number: ")
+# result = float(text)
+#
+# print(result * 2)
+
+
+# float -> int (unsafe)
+
+# float_number = float(input("Enter a float number: "))
+#
+# print(int(float_number))
+
+
+# int -> float
+
+
+# int -> str
+# float -> str
+# a = 42
+# b = str(a)  # "42"
+#
+# type_a = type(a)
+# type_b = type(b)
+#
+# print(type_a)
+# print(type_b)
+
+
+# int -> bool
+
+# a = 3
+#
+# result = bool(a)
+#
+# print(result)
+
+
+# float -> bool
+
+# a = 0.
+#
+# result = bool(a)
+#
+# print(result)
+
+
+# str -> bool
+
+# text = ""
+#
+# result = bool(text)
+#
+# print(result)
+
+
+print(65 % 3)
+
