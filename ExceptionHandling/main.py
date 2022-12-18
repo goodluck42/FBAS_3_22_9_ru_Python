8236255b8112bef683489e842ecd3a8e8bde0f06
Alex <xrlogic1@gmail.com>
2022-12-18 16:50:52 +0400

    *

--- ExceptionHandling/main.py
+++ ExceptionHandling/main.py
@@ -16,47 +16,59 @@
 #     print("An error occurred")
 
 
-# try:
-#     a = int(input("Enter a: "))
-#     op = input("Enter op: ")
-#     b = int(input("Enter b: "))
+try:
+    a = int(input("Enter a: "))
+    op = input("Enter op: ")
+    b = int(input("Enter b: "))
+
+    if op == "/":
+        print(a / b)
+    elif op == "+":
+        print(a + b)
+    else:
+        print("Incorrect operation")
+except ValueError as message:
+    print(message)
+except ZeroDivisionError as message:
+    print(message)
+
+
+# value = 42
+# foo = None
 #
-#     if op == "/":
-#         print(a / b)
-#     elif op == "+":
-#         print(a + b)
-#     else:
-#         print("Incorrect operation")
-# except ValueError as message:
-#     print(message)
-# except ZeroDivisionError as message:
-#     print(message)
+# if value > 0:
+#     foo = 13
+#
+# if value < 0:
+#     foo = 130
+#
+#
+# print(foo)
+#
+# if foo == None:
+#     print("foo is none")
+#
+# print("Hello world")
+#
+#
+# a = 52
+# b = 42
+#
+# if a > 0:
+#     print("positive")
+# elif a < 0:
+#     pass
+# else:
+#     print("equals")
 
 
-value = 42
-foo = None
+val = int(input())
 
-if value > 0:
-    foo = 13
+div_result = val / 100000
 
-if value < 0:
-    foo = 130
+if div_result < 1 or div_result >= 10:
+    print("Not happy")
 
 
-print(foo)
-
-if foo == None:
-    print("foo is none")
-
-print("Hello world")
-
-
-a = 52
-b = 42
-
-if a > 0:
-    print("positive")
-elif a < 0:
-    pass
-else:
-    print("equals")
+# Пользователь вводит 7 чисел
+# Найдите максимальное из них
---
+++ WhileLoop/main.py
@@ -0,0 +1,26 @@
+# i = 0
+#
+# while i < 10:
+#     print(i)
+#     i += 1  # increment
+#
+# print("End of loop")
+
+i = 10
+
+while i > 0:
+    if i == 5:
+        print("i is equals 5")
+    else:
+        print(i)
+    i -= 1  # decrement
+
+print("End of loop")
+
+
+a = 10
+b = 42
+
+# ...
+
+print(a, b)
