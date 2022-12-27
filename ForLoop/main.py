13fe8984a7f96c2117396b5ef0d70f7121b29b2b
Alex <xrlogic1@gmail.com>
2022-12-27 17:27:39 +0400

    *

---
+++ ForLoop/main.py
@@ -0,0 +1,29 @@
+# i = 0
+# # i = 2
+# # j = 2
+# # 0 1 1 2
+# while i < 2:
+#     j = 0
+#     while j < 2:
+#         print(i + j)
+#
+#         j += 1
+#
+#     i += 1
+
+
+# for i in range(10):
+#
+#     if i == 2 or i == 3:
+#         continue
+#     print(i)
+
+
+# students = ["Ibrahim", "Iosif", "Farhad", "Ayhan"]
+#
+# range_1 = range(5)  # 0, 1, 2, 3, 4
+# range_2 = range(3, 7)  # 3, 4, 5, 6
+# range_3 = range(1, 25, 4)  # 1, 5, 9, 13, 17, 21
+#
+# for student in students:
+#     print(student)
